567e11495a116dc41e5803b80641091e3a3341e4
Altan Koc <altankocdev@gmail.com>
2026-08-26 14:29:30 +0300

    initial commit

---
+++ src/main/java/com/routeapp/routebackend/RouteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.routeapp.routebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/routeapp/routebackend/RouteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routeapp.routebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
